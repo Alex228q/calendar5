88b485569005d120d96d8c5b5785420c5800aecb
Aleksei <lena45130@gmail.com>
2023-05-20 12:27:11 +0300

    remove sw

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-window.addEventListener("load", async () => {
-  if ("serviceWorker" in navigator) {
-    try {
-      const reg = await navigator.serviceWorker.register("sw.js");
-    } catch (error) {
-      console.log(`Error =>>> ${error}`);
-    }
-  }
-});
-
-
-
 const calendarEL = document.querySelector(".calendar");
 
 new Calendar({
